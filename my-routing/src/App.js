2f312a4052a2d39a8106853e5bcde01a20e86c9f
SAHARAT TIEWKUNUPAKAN <u6114520@microsoft.au.edu>
2021-02-15 14:28:31 +0700

    add exercise at 15 Feb

---
+++ my-routing/src/App.js
@@ -0,0 +1,14 @@
+import RegistrationForm from './RegistrationForm'
+import { Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+export default function Register() {
+  return (
+
+    <Container>
+      <RegistrationForm />
+    </Container>
+  );
+}
